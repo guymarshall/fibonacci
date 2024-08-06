60a0ecbdb3cdc915cdab78800b5fd4e7a8e17f4d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-08-06 17:51:54 +0100

    UPDATE: Convert to more efficient Rust implementation

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+mod user_input;
+
+extern crate num;
+
+use bigdecimal::BigDecimal;
+use num::BigInt;
+
+fn format_scientific(number: BigInt) -> String {
+    if number < BigInt::from(1000) {
+        return number.to_string();
+    }
+
+    let number_decimal: BigDecimal = BigDecimal::from(number);
+    format!("{:.2E}", number_decimal)
+}
+
+fn main() {
+    let user_input: i32 = user_input::input("Number:");
+
+    let mut first: BigInt = BigInt::from(1);
+    let mut second: BigInt = BigInt::from(1);
+    let mut third: BigInt;
+
+    let mut counter: i32 = 1;
+
+    while counter <= user_input {
+        println!("{counter}");
+
+        third = first + second.clone();
+        first = second;
+        second = third;
+
+        counter += 1;
+    }
+
+    println!("\nfib({}): {}\n", user_input, format_scientific(first));
+}
